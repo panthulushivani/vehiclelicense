60d7636476201b9edb1c2c6c5ede07ad9ac23d78
panthulushivani <pantulushivani@gmail.com>
2022-02-02 14:02:35 +0530

    ds

---
+++ vehiclelicenseproject/src/main/java/com/capg/shiv/vehiclelicense/vehiclelicenseproject/VehiclelicenseprojectApplication.java
@@ -0,0 +1,13 @@
+package com.capg.shiv.vehiclelicense.vehiclelicenseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclelicenseprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclelicenseprojectApplication.class, args);
+	}
+
+}
---
+++ vehiclelicenseproject/src/test/java/com/capg/shiv/vehiclelicense/vehiclelicenseproject/VehiclelicenseprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.shiv.vehiclelicense.vehiclelicenseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclelicenseprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
